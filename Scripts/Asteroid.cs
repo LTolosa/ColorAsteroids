e2df134079c8314624b11fa9b877e75debb99b81
Luis Tolosa <let2120@columbia.edu>
2016-03-26 17:59:52 -0400

    Merging, scene + game controller

---
+++ Scripts/Asteroid.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Asteroid : MonoBehaviour {
+    public Transform player;
+    public GameObject smallAsteroids;
+    public float speed = 1f;
+
+    public int type = 1;
+
+    public Color color;
+
+
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        if (type == 1)
+        {
+            player = GameObject.Find("OVRCameraRig").transform;
+            this.transform.LookAt(player);
+            GetComponent<Rigidbody>().velocity = transform.forward * speed;
+        }
+        else
+        {
+            GetComponent<Rigidbody>().velocity = transform.forward * speed;
+        }
+            
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if(this.transform.position.magnitude > GameController.spawnDistance + 5f)
+        {
+            this.transform.position -= 2 * transform.position;
+        }
+
+	}
+
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Shot"))
+        {
+            Debug.Log("poop");
+            GameObject l_Asteroid = Instantiate(smallAsteroids, this.transform.position, this.transform.rotation) as GameObject;
+            GameObject r_Asteroid = Instantiate(smallAsteroids, this.transform.position, this.transform.rotation) as GameObject;
+            l_Asteroid.transform.Rotate(0, (float)Random.Range(30, 60), 0);
+            r_Asteroid.transform.Rotate(0, (float)Random.Range(-60, -30), 0);
+            l_Asteroid.GetComponent<Rigidbody>().velocity = l_Asteroid.transform.forward * speed/2;
+            r_Asteroid.GetComponent<Rigidbody>().velocity = r_Asteroid.transform.forward * speed/2;
+            l_Asteroid.GetComponent<Asteroid>().type++;
+            r_Asteroid.GetComponent<Asteroid>().type++;
+
+            Destroy(other.gameObject);
+            Destroy(this.gameObject, 1f);
+            
+        }
+        else
+        {
+            return;
+        }
+    }
+   
+
+}
---
+++ Scripts/GameController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject player;
+
+    public GameObject asteroids;
+    public int hazardCount;
+
+    public static float spawnDistance = 25f;
+
+    public float startWait;
+    public float spawnWait;
+    public float waveWait;
+
+    public static bool startWave = true;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(SpawnWaves()); ;
+	}
+	
+	IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true) {
+            GameController.startWave = false;
+            for (int i = 0; i < hazardCount; i++)
+            {
+                Vector3 spawnPosition = Random.insideUnitSphere * spawnDistance;
+                Quaternion spawnRotation = Quaternion.identity;
+                GameObject asteroid = Instantiate(asteroids, spawnPosition, spawnRotation) as GameObject;
+                yield return new WaitForSeconds(spawnWait);
+            }
+
+            while (!GameController.startWave)
+                yield return null;
+        }
+
+    }
+
+
+}
