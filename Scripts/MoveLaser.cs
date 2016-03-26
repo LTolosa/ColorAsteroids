fa0a1507414d4c455733ebb61991a7f98b4259d1
Luis Tolosa <let2120@columbia.edu>
2016-03-26 14:26:50 -0400

    Model and lasers, Volumetric lines

---
+++ Scripts/MoveLaser.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveLaser : MonoBehaviour {
+
+    public float speed = 10f;
+
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = this.GetComponent<Rigidbody>();
+        rb.velocity = transform.forward * speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //this.transform.position += speed * Time.deltaTime * transform.forward;
+        //rb.AddForce(transform.forward * speed * Time.deltaTime);
+	}
+}
---
+++ Scripts/Shoot.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour {
+
+    // Reference to Volumetric Line PreFab
+    public GameObject linePrefab;
+
+    private const float DELAY_TIME = 1f;
+    private float shotTime;
+
+	// Use this for initialization
+	void Start () {
+        shotTime = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButtonDown(0) && shotTime + DELAY_TIME  < Time.time)
+        {
+            shotTime = Time.time;
+
+            GameObject laser = Instantiate(linePrefab, this.transform.position + Vector3.down, Quaternion.Euler(-10, 0, 0)) as GameObject;
+            laser.AddComponent<MoveLaser>();
+            laser.GetComponent<VolumetricLines.VolumetricLineBehavior>().LineWidth = 0.5f;
+        }
+	
+	}
+}
